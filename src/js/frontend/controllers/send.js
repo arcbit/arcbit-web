f7d285a483d3c2e7ed63a0549643358d98b3122f
stequald <stequald@gmail.com>
2016-06-29 18:42:58 -0700

    lower timeoout time to dismiss modal on send

--- src/js/frontend/controllers/send.js
+++ src/js/frontend/controllers/send.js
@@ -226,7 +226,7 @@ define(['./module', 'frontend/port', 'arcbit', 'bitcoinjs-lib', 'model/TLStealth
                                         $scope.$apply();
                                     }
                                 }
-                            }, 13000, false);
+                            }, 5000, false);
                         }, function(response) {
                             $animate.enabled(identity.appDelegate.preferences.getAnimation());
                             modals.cancel();
